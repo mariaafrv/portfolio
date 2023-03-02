f85b970731586e19ca9d5cc14d61d9428e7ea048
mariaafrv <mariaafv@hotmail.com>
2023-03-02 19:42:29 -0300

    retirando comentarios

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const linksAncora = document.querySelectorAll('header a[href^="#"]');
-
-// Para cada link de ancora, adicione um manipulador de eventos de clique
 linksAncora.forEach(link => {
   link.addEventListener('click', (event) => {
-    event.preventDefault(); // Impedir que o link funcione normalmente
+    event.preventDefault(); 
 
-    // Obtenha o elemento que corresponde ao link de ancora clicado
     const destino = document.querySelector(link.hash);
-
-    // Role suavemente até o elemento correspondente
+    
     destino.scrollIntoView({ behavior: 'smooth' });
   });
 });
